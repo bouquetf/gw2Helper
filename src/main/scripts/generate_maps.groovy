4bd6c968cdd299d70ed81a38bb92d3fcab237b8c
bouquetf <bouquet.frederic@gmail.com>
2013-08-05 01:02:55 +0200

    Added script to generate "tp" images

---
+++ src/main/scripts/generate_maps.groovy
@@ -0,0 +1,26 @@
+#! env groovy
+
+
+
+if (args.length != 1) {
+    println """Usage :
+generate_maps [tp_list.csv]
+"""
+    System.exit(1)
+}
+
+
+
+File csv = new File(args[0])
+csv.eachLine {
+    def elements = it.split(";")
+    new File("destination").mkdir();
+    if (elements.length == 3) {
+        generateMap(elements[0], elements[2])
+    }
+}
+
+def generateMap(String id, String originName) {
+    new File("destination/${id}.jpg") << new File("${originName}.jpg").bytes;
+
+}
